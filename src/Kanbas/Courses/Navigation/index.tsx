511943242f54d3b23d2c51ad0db14be1be85f100
haorup <haochemuh@gmail.com>
2024-07-28 22:23:38 -0700

    update navigation page

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -3,14 +3,15 @@ import { Link, useParams, useLocation } from "react-router-dom";
 export default function CoursesNavigation() {
    const { cid } = useParams();
    const { pathname } = useLocation();
-   const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
+   const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
 
 
   return (
 
     <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
       {links.map((link) => (
-         <Link key={`/Kanbas/Courses/${cid}/${link}`} to={`/Kanbas/Courses/${cid}/${link}`} className={`list-group-item list-group-item-action border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}>
+         <Link key={`/Kanbas/Courses/${cid}/${link}`} to={`/Kanbas/Courses/${cid}/${link}`}
+         className={`list-group-item list-group-item-action border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}>
              {link}
 
          </Link>
