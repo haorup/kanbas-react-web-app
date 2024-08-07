f5346dc5c62ad440c33385eb2645abd3d2aa7f96
Hao Pei <37381341+haorup@users.noreply.github.com>
2024-08-06 23:01:07 -0700

    Update App.js

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import cors from "cors";
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
-
+import quizRoutes from './Kanbas/Quizzes/routes.js';
 //
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
@@ -49,4 +49,5 @@ Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+quizRoutes(app);
 app.listen(process.env.PORT || 4000)
