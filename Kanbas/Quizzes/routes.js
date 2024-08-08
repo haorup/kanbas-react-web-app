62589c38c356cfe323c95d8656c2e26f9f6d3478
haorup <haochemuh@gmail.com>
2024-08-08 16:56:51 -0700

    added update and add quiz

--- Kanbas/Quizzes/routes.js
+++ Kanbas/Quizzes/routes.js
@@ -6,4 +6,21 @@ export default function QuizRoutes(app) {
         const quizzes = db.quizzes.filter((m) => m.courses === cid);
         res.json(quizzes);
     });
+
+    app.get("/api/quizzes/:qid", (req, res) => {
+        const {qid} = req.params;
+        const quiz = db.quizzes.find((m) => m._id === qid);
+        res.json(quiz);
+    });
+
+    app.put("/api/quizzes/:qid", (req, res) => {
+        const {qid} = req.params;
+        const quizIndex = db.quizzes.findIndex(
+            (m) => m._id === qid);
+        db.quizzes[quizIndex] = {
+            ...db.quizzes[quizIndex],
+            ...req.body
+        };
+        res.sendStatus(204);
+    });
 }
